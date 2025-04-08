d5397c1f41f5f01478a7e65fae35d78f5987d0cb
pspepp3r <prosperpepple12@gmail.com>
2025-04-08 01:26:11 +0100

    Modified dependency config files and swapped default prophetic image with blogginit logo Defined routes in routes file

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 use Slim\App;
 use Src\Controllers\AuthController;
+use Src\Controllers\BlogController;
+use Src\Controllers\DashboardController;
 use Src\Controllers\LandingController;
 
 return function (App $app): void {
 
     // Homepage
     $app->get('/', [LandingController::class, 'index']);
-    $app->get('/404', [LandingController::class, '_404']);
+    $app->get('/error', [LandingController::class, 'error']);
 
     $app->get('/login', [AuthController::class, 'renderLogin']);
     $app->get('/register', [AuthController::class, 'renderRegister']);
+
+    $app->get('/dashboard', [DashboardController::class, 'index']);
+    $app->get('/reports', [BlogController::class, 'renderReports']);
 };
