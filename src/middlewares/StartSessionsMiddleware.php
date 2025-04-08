eab21004f8b4092f0a7aed7b526deaf6b4b48106
pspepp3r <prosperpepple12@gmail.com>
2025-04-08 20:09:17 +0100

    Added a start session middleware

--- src/middlewares/StartSessionsMiddleware.php
+++ src/middlewares/StartSessionsMiddleware.php
@@ -8,6 +8,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Src\Services\ConfigService;
 use Src\Services\RequestService;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
@@ -15,10 +16,12 @@ class StartSessionsMiddleware implements MiddlewareInterface
 {
     public function __construct(
         private readonly SessionInterface $session,
-        private readonly RequestService $requestService
+        private readonly RequestService $requestService,
+        private readonly ConfigService $config
     ) {}
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
+        $this->session->setName($this->config->get('session.name'));
         $this->session->start();
 
         $response = $handler->handle($request);
@@ -27,7 +30,7 @@ class StartSessionsMiddleware implements MiddlewareInterface
             $this->session->set('previousUrl', (string) $request->getUri());
         }
 
-        // $this->session->save();
+        $this->session->save();
 
         return $response;
     }
